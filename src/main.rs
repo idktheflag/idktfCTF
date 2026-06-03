12e745f5855ea4a1cb98ae2ff5d82b52ff6b16f2
Riverxia <riverdalecoder@outlook.com>
2026-06-03 12:19:50 -0400

    (acm) Currently Listening To: California Gurls - Katy Perry, Snoop Dogg

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod auth;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new(),
-        .route("/health",get(routes::health::handler));
+    let app = Router::new()
+        .route("/health",get(routes::health::handler))
         .route("/auth/register", post(auth::login::register))
         .route("/auth/login", post(auth::login::login));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
