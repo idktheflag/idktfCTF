30a53bf71083584c7520b1fb399e659e10eb64cb
Riverxia <riverdalecoder@outlook.com>
2026-06-03 11:30:56 -0400

    (acm) Currently Listening To: Pink Pony Club - Chappell Roan

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use axum::{Router, routing::get};
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(|| async { "idktheflag CTF platform" }));
+        .route("/health",get(routes::health::handler));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener,app).await.unwrap();
 }
